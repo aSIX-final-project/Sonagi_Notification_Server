8bfa00401faade87384a653d7edf4363cea75d76
NaHyeongJin <skgudwlsdlrj@gmail.com>
2023-11-29 22:33:20 +0900

    나형진
    나형진

---
+++ server.js
@@ -0,0 +1,102 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+
+// Haversine 공식을 이용한 거리 계산 함수
+function getDistance(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
+
+// GET 요청 처리
+app.get('/sendMemberNotification', async (req, res) => {
+    try {
+        const address = req.query.address;
+        const response = await axios.get(`https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURIComponent(address)}`, {
+            headers: {
+                Authorization: 'KakaoAK db06c51425b99419a11f3881f8491642',
+            },
+        });
+        const coordinates = response.data.documents[0].road_address || response.data.documents[0].address;
+        const addressX = parseFloat(coordinates.x);
+        const addressY = parseFloat(coordinates.y);
+
+        console.log(`addressX: ${addressX}, addressY: ${addressY}`);
+
+        const memberResponse = await axios.get("https://port-0-sonagi-app-project-1drvf2lloka4swg.sel5.cloudtype.app/boot/member/findAll");
+        const allMembers = memberResponse.data.list;
+        console.log(allMembers);
+
+        const expoTokens = [];
+
+        for (const member of allMembers) {
+            const memberAddress = member.address;
+            const memberResponse = await axios.get(`https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURIComponent(memberAddress)}`, {
+                headers: {
+                    Authorization: 'KakaoAK db06c51425b99419a11f3881f8491642',
+                },
+            });
+            const memberCoordinates = memberResponse.data.documents[0].road_address || memberResponse.data.documents[0].address;
+            const memberX = parseFloat(memberCoordinates.x);
+            const memberY = parseFloat(memberCoordinates.y);
+            const distance = getDistance(addressY, addressX, memberY, memberX);
+
+            if (distance <= 5) {
+                expoTokens.push(member.expotoken);
+            }
+        }
+
+        await sendExpoPushNotification(expoTokens);
+
+        res.status(200).json({ message: 'Successfully got coordinates and sent messages to members within 5km', coordinates: coordinates });
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+const sendExpoPushNotification = async (tokens) => {
+    for (const expotoken of tokens) {
+        const notification = {
+            to: expotoken,
+            title: '음식 등록 알림',
+            body: '5km 이내 식당에서 음식이 등록되었습니다!',
+            sound: 'default',
+            data: { withSome: 'data' },
+        };
+        try {
+            const response = await axios.post('https://exp.host/--/api/v2/push/send', notification);
+            if (response.status === 200) {
+                console.log(`Successfully sent message to member with token: ${expotoken}`);
+            } else {
+                console.error(`Failed to send message to member with token: ${expotoken}`);
+            }
+        } catch (error) {
+            console.error(`Error sending message to member with token: ${expotoken}, error: ${error.message}`);
+        }
+    }
+};
+
+
+
+// GET 요청 처리
+app.get('/sendResNotification', async (req, res) => {
+    
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
